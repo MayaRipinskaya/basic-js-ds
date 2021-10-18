e183d78548d3d339ca42c9b1fbdf4b2484a9d51b
Maya <majka8@rambler.ru>
2021-10-18 13:28:12 +0300

    feat: add stack

--- src/st-stack.js
+++ src/st-stack.js
@@ -12,19 +12,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
- module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+module.exports = class Stack {
+    constructor() {
+        this.array = []
+    }
+    push(element) {
+        // throw new NotImplementedError('Not implemented');
+        this.array.push(element)
+    }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    pop() {
+        // throw new NotImplementedError('Not implemented');
+        return this.array.pop()
+    }
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    peek() {
+        // throw new NotImplementedError('Not implemented');
+        return this.array[this.array.length - 1]
+    }
 }
